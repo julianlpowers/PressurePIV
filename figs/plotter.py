aed7c44979ce813260a2ed1eb65902eccf9b7095
Julian Powers <julianlpowers@gmail.com>
2024-11-22 14:57:44 -0500

    fix: update plot labels for clarity and add raw data visualization in plotter

--- figs/plotter.py
+++ figs/plotter.py
@@ -12,10 +12,10 @@ plt.plot(x_exact,Cp_exact,'k--',label='exact')
 # plt.plot(data['x'],data['Cp'],label='PIV raw data')
 
 data = np.load(r'AFOIL_extrap_2%c_2%noise.npz')
-plt.plot(data['x'],data['Cp'],label='PIV + linear extrapolation')
+plt.plot(data['x'],data['Cp'],label='PIV + linear extrapolation from 0.02c')
 
 data = np.load(r'AFOIL_streamlines_3%c_2%noise.npz')
-plt.plot(data['x'],data['Cp'],label='PIV + streamline method')
+plt.plot(data['x'],data['Cp'],label='PIV + streamline method from 0.02c')
 
 plt.xlabel('x/c'); plt.ylabel('Cp'); plt.legend(); plt.grid(); plt.gca().invert_yaxis(); plt.title('A-Airfoil')
 
@@ -30,17 +30,22 @@ plt.figure('CYLINDER')
 data = pd.read_excel(r'10_8_24.xlsx')
 theta_taps = data['theta.1'].to_numpy()
 Cp_taps = data['Cp.1'].to_numpy()
-plt.errorbar(theta_taps, Cp_taps, yerr=0.03, fmt='k.', mfc='none', barsabove=True, label='taps', capsize=5)
+plt.errorbar(theta_taps, Cp_taps, yerr=0.03, fmt='k.', mfc='none', barsabove=True, label='pressure taps', capsize=5)
 
 
 # data = np.load(r'CYLINDER_raw_0mm.npz')
 # plt.plot(data['theta'],data['Cp'],label='PIV raw data')
 
 data = np.load(r'CYLINDER_extrap_2mm.npz')
-plt.plot(data['theta'],data['Cp'],label='PIV + linear extrapolation')
+plt.plot(data['theta'],data['Cp'],label='PIV + linear extrapolation from 0.04D')
 
 data = np.load(r'CYLINDER_streamlines_2mm.npz')
-plt.plot(data['theta'],data['Cp'],label='PIV + streamline method')
+plt.plot(data['theta'],data['Cp'],label='PIV + streamline method from 0.04D')
+
+data = np.load(r'CYLINDER_raw_0mm.npz')
+plt.plot(data['theta'],data['Cp'],label='PIV + all data used no extrapolation')
+
+
 
 plt.xlabel(r'$\theta\;[^\circ]$'); plt.ylabel('Cp'); plt.legend(); plt.grid(); plt.title('Cylinder')
 
